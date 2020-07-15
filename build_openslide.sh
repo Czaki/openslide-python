a3b4ee8cd743e59465ee0266ea3f19de14ccde03
Grzegorz Bokota <bokota+github@gmail.com>
2020-07-15 21:16:05 +0200

    end on build fail

--- build_openslide.sh
+++ build_openslide.sh
@@ -1,4 +1,6 @@
-wget https://github.com/openslide/openslide/releases/download/v3.4.1/openslide-3.4.1.tar.gz
+set -e
+
+curl https://github.com/openslide/openslide/releases/download/v3.4.1/openslide-3.4.1.tar.gz
 
 tar zxvf openslide-3.4.1.tar.gz
 
