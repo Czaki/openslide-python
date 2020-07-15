2c12f56e098793d20752c4f9dc22d8f33f22ba9a
Grzegorz Bokota <bokota+github@gmail.com>
2020-07-15 19:06:57 +0200

    initial configuration for build wheel

---
+++ .github/workflows/build_openslide.sh
@@ -0,0 +1,9 @@
+wget https://github.com/openslide/openslide/releases/download/v3.4.1/openslide-3.4.1.tar.gz
+
+tar zxvf openslide-3.4.1.tar.gz
+
+cd openslide-3.4.1
+
+./configure
+make
+make install
